a922c617146d06e8dad5e61b80c6528dd4015817
CrisAriasQ <163191457+CrisAriasQ@users.noreply.github.com>
2024-07-13 22:11:18 -0400

    Item 7

--- router_ei.py
+++ router_ei.py
@@ -1,6 +1,6 @@
 from netmiko import ConnectHandler
 
-# Configuración de conexión al router
+
 router = {
     'device_type': 'cisco_ios',
     'host': '192.168.56.101',
@@ -8,11 +8,11 @@ router = {
     'password': 'cisco123!',
 }
 
-# Conectar al router
+
 net_connect = ConnectHandler(**router)
 net_connect.enable()
 
-# Configuración de EIGRP Nombrado
+
 eigrp_config = [
     'router eigrp EIGRP_TEST',
     'address-family ipv4 unicast autonomous-system 1',
@@ -30,24 +30,24 @@ eigrp_config = [
 
 net_connect.send_config_set(eigrp_config)
 
-# Obtener configuración EIGRP
+
 output = net_connect.send_command('show running-config | section eigrp')
 print("EIGRP Configuration:\n", output)
 
-# Obtener información de las IP y estado de las interfaces
+
 output = net_connect.send_command('show ip interface brief')
 print("IP Interface Brief:\n", output)
 
 output = net_connect.send_command('show ipv6 interface brief')
 print("IPv6 Interface Brief:\n", output)
 
-# Obtener running-config
+
 output = net_connect.send_command('show running-config')
 print("Running Config:\n", output)
 
-# Obtener show version
+
 output = net_connect.send_command('show version')
 print("Version Information:\n", output)
 
-# Cerrar la conexión
+
 net_connect.disconnect()
